ba89721fb2c036f18a0b90d347d125f8218cf2c5
phsantiago <pedroh_s_@hotmail.com>
2018-01-26 01:44:08 +0000

    Create react app with hot reload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.css';
+
+function Header() {
+  return (
+    <div className={styles.base}>
+    </div>
+  );
+}
+
+
+export default Header;
